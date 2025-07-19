6aa2b664da4f3ee8c24c8ed04a1a16b01dfc65fa
Yulia <derevyanko.julia@gmail.com>
2025-07-19 18:37:02 +0300

    Remove validation checks for createContact service: simplify code by eliminating payload validation and error handling for missing fields

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -17,16 +17,6 @@ export const getContactByIdService = async (contactId) => {
 };
 // POST
 export const createContact = async (payload) => {
-  if (!payload) {
-    throw createError(400, 'Missing request body');
-  }
-  const { name, phoneNumber, contactType } = payload;
-  if (!name || !phoneNumber || !contactType) {
-    throw createError(
-      400,
-      'Missing required fields: name, phoneNumber, or contactType',
-    );
-  }
   const contact = new Contact(payload);
   await contact.save();
   return contact;
